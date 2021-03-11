36ec22df17eeb43b44bdd5b8a5c472d16c9c1471
Jason Rothstein <jason.rothstein@oracle.com>
2021-03-10 19:09:23 -0600

    Update terraform providers and handle 0.14 provider locks

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     oci = {
       source  = "hashicorp/oci"
-      version = ">= 4.7.0"
+      version = ">= 4.17.0"
     }
     random = {
       source  = "hashicorp/random"
